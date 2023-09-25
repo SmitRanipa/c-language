ac0ddd6adb1c4256ed803cb54368d415e5740548
SmitRanipa <141202936+SmitRanipa@users.noreply.github.com>
2023-09-25 10:39:04 +0530

    Add files via upload

---
+++ Lab 8/8-B-1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+	int i=0;
+	float root;
+	
+	while(i<=9)
+	{
+		printf("%f\n", sqrt(i));
+		i++;
+	}
+}
---
+++ Lab 8/8-B-3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int count=1,b=1;
+	
+	while(count<51)
+	{
+		printf("%d , " ,b);
+		b=b+3;
+		count++;
+	}
+}
